d080b3c6ef64404d6897be2d54ec4495cdd8bd81
Ryan Shipwash <rmshipwash42@tntech.edu>
2024-10-29 13:34:00 -0500

    Changed users.h to linked list class

--- users.h
+++ users.h
@@ -9,22 +9,37 @@ using namespace std;
 class UserStorage
 {
     private:
-        UserData** users;
-        int MAXSIZE;
-        int numProfiles;
+        //Declare structure for linked list
+        struct UserNode
+        {
+            UserData value;
+            UserNode* next; //Points to next node
+        };
+
+        UserNode* head; //Head pointer
+        UserNode* tail; //Tail pointer
 
     public:
-        UserStorage(int MAXSIZE);
+        //Constructor
+        UserStorage()
+        {
+            head = NULL;
+            tail = NULL;
+        }
 
+        //Destructor
         ~UserStorage();
 
-        void readData(const string& userData);
-        void printAllUsers();
-        void addUser(UserData* user);
-        
-        int getNumUsers() const;
-        UserData* getUser(int index) const;
-
+        //Linked List operations
+        bool isEmpty();
+		int getLength();
+		int search(UserData);
+		UserData getNodeValue(UserData);
+		void appendNode(UserData);
+		void insertNode(int, UserData);
+		void removeNode(UserData);
+		void displayList() const;
+        void sortList();
 };
 
 #endif
